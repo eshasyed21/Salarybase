a858f5b8091f41d3235c0bd457dd6ec744dd26ae
eshasyed21 <94787665+eshasyed21@users.noreply.github.com>
2021-11-29 23:19:38 -0800

    salary base

---
+++ SalaryBaseSystem/SalaryBaseSystem/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SalaryBaseSystem
+{
+       public class Employee
+    {
+        private int Employee_ID;
+        private int Employee_Name;
+        private int Employee_Salary;
+        private int HoursWork;
+        private int UnitProduce;
+
+        public void C()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
+
+
+        public void Calculate_Salary()
+        {
+          if(hoursWorked>Base_Hours)
+    {
+        basePay = hourlyPayRate * BASE_Hours;
+        overtimeHours = hoursWorked - BASE_Hours;
+        overtimePay = overtimeHours * hourlyPayRate * OverTIME_RATE;
+        unitPay = 3 * unitproduced;
+        Grosspay = basePay + overtimePay + unitpay;
+    }
+        }
+    
